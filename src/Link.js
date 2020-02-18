e0f690cb9a06883a2a0bfd03f7dad1c6d4defdc5
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2020-02-17 23:17:17 -0300

    feat(init): ༼ つ ◕_◕ ༽つGraphQL & Relay Concepts

---
+++ src/Link.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const Link = styled.a`
+  color: #25D7FD;
+`
---
+++ src/code/setState.tsx
@@ -0,0 +1,17 @@
+class App extends React {
+  state = {
+    isLoading: true,
+  };
+
+  render() {
+    const { isLoading } = this.state;
+
+    if (isLoading) {
+      return <Loading />;
+    }
+
+    return (
+      <Root />
+    );
+  }
+}
